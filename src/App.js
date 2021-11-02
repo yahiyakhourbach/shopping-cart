372ec5e08a197facfb3cfa66517a83a5571adbc1
yahiyakhourbach <yahyakharbach9@gmail.com>
2021-11-02 13:43:08 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="header">
+        <a href="/">Shopping Cart</a>
+      </header>
+      <main className="main">this is the content</main>
+      <footer className="footer"> all Right is reserved</footer>
+    </div>
+  );
+}
+
+export default App;
