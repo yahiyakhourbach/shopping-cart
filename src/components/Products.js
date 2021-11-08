5e7ae6ebe13f434a77d4508f9560d2952b631a6b
yahiyakhourbach <yahyakharbach9@gmail.com>
2021-11-08 16:15:21 +0000

    component-animation

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,32 +1,104 @@
 import React, { Component } from 'react';
-
+import Fade from 'react-reveal/Fade';
+import Zoom from 'react-reveal/Zoom';
+import Modal from 'react-modal';
+Modal.setAppElement('#root');
 class Products extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { product: null };
+  }
+  openModel = (product) => {
+    this.setState({ product });
+  };
+  closeModel = () => {
+    this.setState({ product: null });
+  };
   render() {
+    const { product } = this.state;
     return (
       <div>
-        <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <a href={`# ${product._id}`}>
-                  <img src={product.image} alt={product.title} />
-                  <p> {product.title}</p>
-                </a>
-                <div className="product-price">
-                  <div> {product.price}$</div>
-                  <button
-                    className="button primary"
+        <Fade bottom cascade>
+          <ul className="products">
+            {this.props.products.map((product) => (
+              <li key={product._id}>
+                <div className="product">
+                  <a
+                    href={`#${product._id}`}
                     onClick={() => {
-                      this.props.AddToCart(product);
+                      this.openModel(product);
                     }}
                   >
-                    Add To Cart
-                  </button>
+                    <img src={product.image} alt={product.title} />
+                    <p> {product.title}</p>
+                  </a>
+                  <div className="product-price">
+                    <div> {product.price}$</div>
+                    <button
+                      className="button primary"
+                      onClick={() => {
+                        this.props.AddToCart(product);
+                      }}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Fade>
+        {product && (
+          <Modal
+            isOpen={true}
+            onRequestClose={this.closeModel}
+            style={{
+              overlay: { backgroundColor: 'gray' },
+            }}
+          >
+            <Zoom>
+              <button
+                className="model-close"
+                onClick={() => {
+                  this.closeModel();
+                }}
+              >
+                X
+              </button>
+              <div className="product-container">
+                <div className="product-img">
+                  <img src={product.image} alt={product.title} />
+                </div>
+                <div class="product-detailes">
+                  <h3>{product.title}</h3>
+                  <p>{product.description}</p>
+                  <div>
+                    Available Sizes:{' '}
+                    {product.availablesize.map((x) => {
+                      return (
+                        <span>
+                          <button>{x}</button>{' '}
+                        </span>
+                      );
+                    })}
+                  </div>
+                  <div className="product-modal-price">
+                    <div>{product.price}$</div>
+                    <button
+                      className="primary"
+                      onClick={() => {
+                        this.props.AddToCart(product);
+                        this.closeModel();
+                      }}
+                    >
+                      Add To Card
+                    </button>
+                  </div>
                 </div>
               </div>
-            </li>
-          ))}
-        </ul>
+            </Zoom>
+          </Modal>
+        )}
       </div>
     );
   }
